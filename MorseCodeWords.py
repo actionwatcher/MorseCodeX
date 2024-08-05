6936ed41fd7f5cfde199f242c69124d5fb410acc
Arkady Ten <arkadyten@yahoo.com>
2024-08-05 15:39:12 -0700

    Refactoring: separate Datasource

--- MorseCodeWords.py
+++ MorseCodeWords.py
@@ -9,6 +9,7 @@ from SessionDB import Session, SessionDB
 from NoiseSoundSource import NoiseSoundSource
 from Mixer import Mixer
 from MorseSoundSource import MorseSoundSource
+from DataSource import DataSource
 import numpy as np
 import wave
 import time
@@ -411,45 +412,6 @@ class MorseTrainerUI:
         self.player.stop()
         self.save_settings()
         self.root.quit()
-
-import random
-
-class DataSource:
-    def __init__(self, file_path='MASTER.SCP', num_words=50):
-        self.words = self._load_words(file_path)
-        self.num_words = num_words
-        self.reset()
-
-    def _load_words(self, file_path):
-        words = []
-        try:
-            with open(file_path, 'r') as file:
-                for line in file:
-                    line = line.strip()
-                    if line and not line.startswith('#'):
-                        words.append(line)
-        except FileNotFoundError:
-            print(f"File {file_path} not found.")
-        return words
-    
-    def reset(self):
-        if self.num_words <= len(self.words): # pefer unique selection
-            self.selected_words = random.sample(self.words, k = self.num_words)
-        else:
-            self.selected_words = random.choices(self.words, k=self.num_words)
-        self.index = 0
-
-    def get_next_word(self):
-        if self.index >= len(self.selected_words):
-            self.index = 0
-            return None
-        word = self.selected_words[self.index]
-        self.index += 1
-        return word
-
-# Example usage
-# data_source = DataSource(file_path='MASTER.SCP', num_words=10)
-# print(data_source.get_next_word())
 
 
 def compare(sent_word, received_word):
